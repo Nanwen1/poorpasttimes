83b9b20e262e3bbaa8f9ef41f93e31c45ecd2f83
Nanwen <nanwens@gmail.com>
2019-10-05 11:48:37 +0800

    AWS Attempt 2

--- eventFinderProject/settings.py
+++ eventFinderProject/settings.py
@@ -110,6 +110,19 @@ DATABASES = {
     }
 }
 
+# Use environment settings for database
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
